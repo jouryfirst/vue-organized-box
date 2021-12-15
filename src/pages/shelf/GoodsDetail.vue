8d9997fd8a9f1c54353b727e2e80eecb337fb2fc
jouryzhang <jouryzhang@gmail.com>
2021-12-16 07:52:35 +0800

    增加物品详情页

---
+++ src/pages/shelf/GoodsDetail.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="goods-detail-container">
+        <j-panel :title="title" @return="returnRoute"></j-panel>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "GoodsDetail",
+    data () {
+      return {
+        title: ''
+      }
+    },
+    methods: {
+      returnRoute () {
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
